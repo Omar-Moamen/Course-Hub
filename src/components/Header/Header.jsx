99c3a09185083235cfe1c0e1b5f09094b3aebaf4
OmarMoamen <omarmoamen911@gmail.com>
2024-01-23 20:32:18 +0200

    fixed some performance issues

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,8 +2,6 @@ import {Container, Nav, Navbar} from "react-bootstrap";
 import {useEffect, useState} from "react";
 import './Header.css';
 import {Link, NavLink, useLocation} from "react-router-dom";
-import {faBars} from "@fortawesome/free-solid-svg-icons";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 export default function Header()
 {
@@ -17,7 +15,6 @@ export default function Header()
    useEffect(() =>
    {
       window.addEventListener('resize', () => setScreenSize(window.innerWidth))
-      return () => window.addEventListener('resize', setScreenSize(window.innerWidth))
    }, [screenSize])
 
    const scrollHandler = () =>
@@ -44,23 +41,25 @@ export default function Header()
          ${showNavbar || screenSize < 992 || pathname !== "/" ?
                "bg-light text-black alt-nav-hover main-shadow" :
                "bg-transparent nav-color"} fixed-top py-3`}
-         expand="lg">
+         expand="lg"
+      >
          <Container className={`${screenSize < 992 ? "bg-light" : null} px-4 px-lg-5`}>
             <Link to={'/'} className="navbar-brand">Logo</Link>
             <Navbar.Toggle
                className={`${toggler ? "rotate-bars" : ""} border-0 position-relative rounded-0`}
                aria-controls="basic-navbar-nav"
-               onClick={() => setToggler(!toggler)}>
+               onClick={() => setToggler(!toggler)}
+            >
                <span></span>
                <span></span>
                <span></span>
             </Navbar.Toggle>
             <Navbar.Collapse
                id="basic-navbar-nav"
-               className={`${screenSize < 992 ? "bg-light px-2 w-100" : null}`}
+               className={`${screenSize < 992 ? "bg-light" : null}`}
             >
                <Nav
-                  className={`${screenSize < 992 ? "text-black-50" : null} ms-auto gap-4 fw-bold py-4 mw-100`}
+                  className={`${screenSize < 992 ? "text-black-50" : null} me-auto me-lg-0 ms-lg-auto gap-4 fw-bold py-4`}
                >
                   <NavLink to="/">About</NavLink>
                   <NavLink to="#action2">Services</NavLink>
